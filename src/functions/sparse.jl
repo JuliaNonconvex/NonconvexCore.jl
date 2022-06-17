1e31517a88fbc78c84276ecc1fdaa7e04a0b9125
Mohamed Tarek <mohamed82008@gmail.com>
2022-06-18 03:16:43 +1000

    sparsity improvements

--- src/functions/sparse.jl
+++ src/functions/sparse.jl
@@ -1,39 +1,79 @@
 # workaround Zygote.jacobian and ForwardDiff.jacobian not supporting sparse jacobians
-
+function sparse_gradient(f, x)
+    _, pb = Zygote.pullback(f, x)
+    return _sparsevec(pb(1.0)[1])
+end
 function sparse_jacobian(f, x)
     val, pb = Zygote.pullback(f, x)
-    M, N = length(val), length(x)
-    T = eltype(val)
-    return copy(mapreduce(hcat, 1:M, init = spzeros(T, N, 0)) do i
-        sparse(pb(sparsevec([i], [true], N))[1])
-    end')
+    M = length(val)
+    vecs = [sparsevec([i], [true], M) for i in 1:M]
+    Jt = reduce(hcat, first.(pb.(vecs)))
+    return copy(Jt')
 end
-function sparse_hessian(f, x)
-    return sparse_fd_jacobian(x -> Zygote.gradient(f, x)[1], x)
-end
-
 function sparse_fd_jacobian(f, x)
     pf = pushforward_function(f, x)
-    M, N = length(f(x)), length(x)
-    init = pf(I(M)[:, 1])[1]
-    M = length(init)
-    return mapreduce(hcat, 2:N; init) do i
-        sparse(pf(sparsevec([i], [true], M))[1])
+    M = length(x)
+    vecs = [sparsevec([i], [true], M) for i in 1:M]
+    # assumes there will be no structural non-zeros that have value 0
+    return reduce(hcat, sparse.(pf.(vecs)))
+end
+function pushforward_function(f, x)
+    return v -> begin
+        return ForwardDiff.derivative(h -> f(x + h * v), 0)
     end
 end
 
-# from AbstractDifferentiation
-function pushforward_function(f, xs...)
-    return function pushforward(vs)
-        if length(xs) == 1
-            v = vs isa Tuple ? only(vs) : vs
-            return (ForwardDiff.derivative(h -> f(step_toward(xs[1], v, h)), 0),)
-        else
-            return ForwardDiff.derivative(h -> f(step_toward.(xs, vs, h)...), 0)
+function sparse_hessian(f, x)
+    return sparse_fd_jacobian(x -> sparse_gradient(f, x), x)
+end
+
+_sparsevec(x::Real) = [x]
+_sparsevec(x::Vector) = copy(x)
+_sparsevec(x::Matrix) = copy(vec(x))
+_sparsevec(x::SparseVector) = x
+function _sparsevec(x::Adjoint{<:Real, <:AbstractMatrix})
+    return _sparsevec(copy(x))
+end
+function _sparsevec(x::SparseMatrixCSC)
+    m, n = size(x)
+    linear_inds = zeros(Int, length(x.nzval))
+    count = 1
+    for colind in 1:length(x.colptr)-1
+        for ind in x.colptr[colind]:x.colptr[colind+1]-1
+            rowind = x.rowval[ind]
+            val = x.nzval[ind]
+            linear_inds[count] = rowind + (colind - 1) * m
+            count += 1
         end
     end
+    return sparsevec(linear_inds, copy(x.nzval), prod(size(x)))
 end
 
-@inline step_toward(x::Number, v::Number, h) = x + h * v
-# support arrays and tuples
-@noinline step_toward(x, v, h) = x .+ h .* v
+_sparse_reshape(v::AbstractVector, _) = v
+_sparse_reshape(v::Vector, m, n) = reshape(v, m, n)
+function _sparse_reshape(v::SparseVector, m, n)
+    if length(v.nzval) == 0
+        return sparse(Int[], Int[], v.nzval, m, n)
+    end
+    N = length(v.nzval)
+    I = zeros(Int, N)
+    J = zeros(Int, N)
+    for ind in v.nzind
+        _col, _row = divrem(ind, m)
+        if _row == 0
+            col = _col
+            row = m
+        else
+            col = _col + 1
+            row = _row
+        end
+        I[ind] = row
+        J[ind] = col
+    end
+    return sparse(I, J, copy(v.nzval), m, n)
+end
+
+function ChainRulesCore.rrule(::typeof(_sparsevec), x)
+    val = _sparsevec(x)
+    val, Δ -> (NoTangent(), _sparse_reshape(Δ, size(x)...))
+end
--- src/models/vec_model.jl
+++ src/models/vec_model.jl
@@ -71,23 +71,41 @@ function optimize(model::VecModel, optimizer::AbstractOptimizer, args...; kwargs
     return optimize!(workspace)
 end
 
+function tovecfunc(f, x...; flatteny = true)
+    vx, _unflattenx = flatten(x)
+    unflattenx = Unflatten(x, _unflattenx)
+    if flatteny
+        y = f(x...)
+        tmp = maybeflatten(y)
+        # should be addressed in maybeflatten
+        if y isa Real
+            unflatteny = identity
+        else
+            unflatteny = Unflatten(y, tmp[2])
+        end
+        return x -> maybeflatten(f(unflattenx(x)...))[1], float.(vx), unflatteny
+    else
+        return x -> f(unflattenx(x)...), float.(vx), identity
+    end
+end
+
 function tovecmodel(m::AbstractModel, x0 = deepcopy(getmin(m)))
     v, _unflatten = flatten(x0)
     unflatten = Unflatten(x0, _unflatten)
     return VecModel(
         # objective
-        Objective(x -> m.objective(unflatten(x)), flags = m.objective.flags),
+        Objective(tovecfunc(m.objective.f, x0)[1], m.objective.multiple, m.objective.flags),
         # eq_constraints
         length(m.eq_constraints.fs) != 0 ? VectorOfFunctions(map(m.eq_constraints.fs) do c
-            EqConstraint(x -> maybeflatten(c.f(unflatten(x)))[1], maybeflatten(c.rhs)[1], c.dim, c.flags)
+            EqConstraint(tovecfunc(c.f, x0)[1], maybeflatten(c.rhs)[1], c.dim, c.flags)
         end) : VectorOfFunctions(EqConstraint[]),
         # ineq_constraints
         length(m.ineq_constraints.fs) != 0 ? VectorOfFunctions(map(m.ineq_constraints.fs) do c
-            IneqConstraint(x -> maybeflatten(c.f(unflatten(x)))[1], maybeflatten(c.rhs)[1], c.dim, c.flags)
+            IneqConstraint(tovecfunc(c.f, x0)[1], maybeflatten(c.rhs)[1], c.dim, c.flags)
         end) : VectorOfFunctions(IneqConstraint[]),
         # sd_constraints
         length(m.sd_constraints.fs) != 0 ? VectorOfFunctions(map(m.sd_constraints.fs) do c
-            SDConstraint(x -> c.f(unflatten(x)), c.dim)
+            SDConstraint(tovecfunc(c.f, x0; flatteny = false)[1], c.dim)
         end) : VectorOfFunctions(SDConstraint[]),
         # box_min
         float.(flatten(m.box_min)[1]),
@@ -99,4 +117,3 @@ function tovecmodel(m::AbstractModel, x0 = deepcopy(getmin(m)))
         convert(BitVector, flatten(m.integer)[1]),
     ), float.(v), unflatten
 end
-
