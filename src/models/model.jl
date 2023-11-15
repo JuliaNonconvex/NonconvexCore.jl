33c55180df6769fda6efb8b24c17bd896cb0623b
Mohamed Tarek <mohamed82008@gmail.com>
2023-11-15 03:16:27 +0200

    formatting

--- src/models/model.jl
+++ src/models/model.jl
@@ -232,7 +232,11 @@ function add_eq_constraint!(m::AbstractModel, fs::Vector{<:EqConstraint})
     return m
 end
 
-function add_sd_constraint!(m::AbstractModel, f::Function, dim = size(f(reduce_type(getinit(m))), 1))
+function add_sd_constraint!(
+    m::AbstractModel,
+    f::Function,
+    dim = size(f(reduce_type(getinit(m))), 1),
+)
     return add_sd_constraint!(m, SDConstraint(f, dim))
 end
 function add_sd_constraint!(m::AbstractModel, sd_constraint::AbstractFunction)
